d8601e5b3488fd2f22528bb74a3decf3ae626860
Tomiaa <1178852449@qq.com>
2022-11-06 12:56:17 +0800

    feat: 调整文件目录和路径

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -13,39 +13,83 @@ export default defineConfig({
     nav: [
       {
         text: "html",
-        link: "/html/1-了解HTML",
+        items: [
+          { text: "html介绍", link: "/html/html介绍/1-了解html" },
+          {
+            text: "多媒体与嵌入",
+            link: "/html/多媒体与嵌入/1-了解多媒体与嵌入",
+          },
+          { text: "html表格", link: "/html/html表格/1-了解表格" },
+        ],
       },
       {
         text: "javascript",
-        link: "/javascript/1-了解javascript",
+        items: [
+          {
+            text: "javascript第一步",
+            link: "/javascript/javascript第一步/1-了解javascript",
+          },
+        ],
       },
     ],
     sidebar: {
-      "/html/": [
+      "/html": [
         {
-          text: "HTML",
+          text: "html",
+          collapsible: true,
           items: [
-            { text: "了解HTML", link: "/html/1-了解HTML" },
-            { text: "head详解", link: "/html/2-head详解" },
-            { text: "常用的html元素", link: "/html/3-常用的html元素" },
-            { text: "img元素", link: "/html/4-img元素" },
-            { text: "超链接a元素", link: "/html/5-超链接a元素" },
-            { text: "高阶文字排版", link: "/html/6-高阶文字排版" },
-            { text: "文档与网站架构", link: "/html/7-文档与网站架构" },
+            { text: "了解html", link: "/html/html介绍/1-了解html" },
+            { text: "head详解", link: "/html/html介绍/2-head详解" },
+            { text: "常用的html元素", link: "/html/html介绍/3-常用的html元素" },
+            { text: "img元素", link: "/html/html介绍/4-img元素" },
+            { text: "超链接a元素", link: "/html/html介绍/5-超链接a元素" },
+            { text: "高阶文字排版", link: "/html/html介绍/6-高阶文字排版" },
+            { text: "文档与网站架构", link: "/html/html介绍/7-文档与网站架构" },
+          ],
+        },
+        {
+          text: "多媒体与嵌入",
+          collapsible: true,
+          items: [
+            {
+              text: "了解多媒体与嵌入",
+              link: "/html/多媒体与嵌入/1-了解多媒体与嵌入",
+            },
+          ],
+        },
+
+        {
+          text: "html表格",
+          collapsible: true,
+          items: [
+            {
+              text: "了解表格",
+              link: "/html/html表格/1-了解表格",
+            },
           ],
         },
       ],
       "/javascript/": [
         {
           text: "javascript",
+          collapsible: true,
           items: [
-            { text: "了解javascript", link: "/javascript/1-了解javascript" },
-            { text: "变量", link: "/javascript/2-变量" },
-            { text: "操作符", link: "/javascript/3-操作符" },
-            { text: "字符串", link: "/javascript/4-字符串" },
-            { text: "字符串方法", link: "/javascript/5-字符串方法" },
-            { text: "数组", link: "/javascript/6-数组" },
-            { text: "数组方法", link: "/javascript/7-数组方法" },
+            {
+              text: "了解javascript",
+              link: "/javascript/javascript第一步/1-了解javascript",
+            },
+            { text: "变量", link: "/javascript/javascript第一步/2-变量" },
+            { text: "操作符", link: "/javascript/javascript第一步/3-操作符" },
+            { text: "字符串", link: "/javascript/javascript第一步/4-字符串" },
+            {
+              text: "字符串方法",
+              link: "/javascript/javascript第一步/5-字符串方法",
+            },
+            { text: "数组", link: "/javascript/javascript第一步/6-数组" },
+            {
+              text: "数组方法",
+              link: "/javascript/javascript第一步/7-数组方法",
+            },
           ],
         },
       ],
